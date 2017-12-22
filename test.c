b2b97bc7dcfbc39afa0ca643a4fb8f322216ec24
isseebx123 <lsseebx38@gmail.com>
2017-12-23 00:35:41 +0900

    ch test case

--- test.c
+++ test.c
@@ -1,8 +1,9 @@
-
-int t[1];
+double t[1];
 double aa[1];
 float bb[2];
-double x;
+int x;
+double cc = 1.1;
+int bb = 5;
 void main () 
 { 
 
@@ -10,11 +11,11 @@ void main ()
     int y;
    	int j = 1;
 	while(x>0){
-		y=0.2;
-		t[9.2] = 1;
+		y=0;
+		t[9] = 1.2;
 	}
 
-    for( x=0; x<3; x = x+1.1 ){
+    for( x=0; x<3; x = x+1 ){
 	int z = 1;
 	++y;
     }
